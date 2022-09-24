0b1b83c7ed0495da54678a58d0f249198a68de1b
John <wheeler6123@gmail.com>
2022-09-24 11:49:49 -0600

    refactored code to fit within an MVC architecture model

---
+++ routes/edit.js
@@ -0,0 +1,12 @@
+//Handles editing and deleting items and rendering the edit page itself
+
+const express = require('express')
+const router = express.Router()
+const editController = require('../controllers/edit')
+
+router.get('/:id', editController.getEdit)
+router.get('/remove/:id', editController.deleteTask)
+router.post('/:id', editController.updateTask)
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -8,83 +8,23 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 const mongoose = require("mongoose");
-const TodoTask = require("./models/TodoTask");
-require('dotenv').config()
+const connectDB = require('./config/database')
+const TodoTask = require("./models/todotask");
+const homeRoutes = require('./routes/home');
+const editRoutes = require('./routes/edit')
+require('dotenv').config({path: './config/.env'})
+
+//Connect to MongoDB
+connectDB()
 
 //Set Middleware
 app.set("view engine", "ejs");
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 
-//Connect to Mongo
-mongoose.connect(
-    process.env.DB_CONNECTION, 
-    { useNewUrlParser: true }, 
-    () => {console.log("Connected to db!");}
-)
-
-// GET METHOD
-app.get("/", async (req, res) => {
-    try {
-        TodoTask.find({}, (err, tasks) => {
-            res.render("index.ejs", { todoTasks: tasks });
-        });
-    } catch (err) {
-        if (err) return res.status(500).send(err);
-    }
-});
-
-//POST METHOD
-app.post('/', async (req, res) => {
-    const todoTask = new TodoTask(
-        {
-            title: req.body.title,
-            content: req.body.content
-        });
-    try {
-        await todoTask.save();
-        console.log(todoTask)
-        res.redirect("/");
-    } catch (err) {
-        if (err) return res.status(500).send(err);
-        res.redirect("/");
-    }
-});
-
-//UPDATE METHOD
-app
-    .route("/edit/:id")
-    .get((req, res) => {
-        const id = req.params.id;
-        TodoTask.find({}, (err, tasks) => {
-            res.render("edit.ejs", { todoTasks: tasks, idTask: id });
-        });
-    })
-    .post((req, res) => {
-        const id = req.params.id;
-        TodoTask.findByIdAndUpdate(
-            id,
-            {
-                title: req.body.title,
-                content: req.body.content
-            },
-
-            err => {
-                if (err) return res.status(500).send(err);
-                res.redirect("/");
-            });
-    });
-
-//DELETE
-app
-    .route("/remove/:id")
-    .get((req, res) => {
-        const id = req.params.id;
-        TodoTask.findByIdAndRemove(id, err => {
-            if (err) return res.send(500, err);
-            res.redirect("/");
-        });
-    });
+//Routes
+app.use('/', homeRoutes)
+app.use('/edit', editRoutes)
 
 //Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
